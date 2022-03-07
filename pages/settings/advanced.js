81a7a08c19c51a3122156d824184a32622993ee6
Gizem <gizem.candemir@secilstore.com>
2022-03-07 14:23:48 +0300

    Material UI added, created permissions

--- pages/settings/advanced.js
+++ pages/settings/advanced.js
@@ -1,9 +1,10 @@
-import { Grid, Typography, Card, CardContent} from '@mui/material'
+import { Grid, Typography, Card, CardContent, CardMedia} from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
-import styles from '../../styles/Advanced.module.css'
-
+import TableHeader from '../../components/tableHeader/TableHeader.js'
 const advanced = () => {
+  const data= [{name: 'Add User', url: '/sidebarIcon/plus.png'}, {name: 'Add Role', url: '/sidebarIcon/plus.png'}]
+
   return (
      
     <Grid container spacing={2}>
@@ -17,27 +18,43 @@ const advanced = () => {
 
        <Grid item xs={12} md={12}>
          <Card sx={{ display: 'flex' }}>
+         <CardMedia
+        component="img"
+        sx={{ width: 120 }}
+        image="/sidebarIcon/account.png"
+        alt="Live from space album cover"
+      />
           <Box sx={{ display: 'flex', flexDirection: 'column' }}>
             <CardContent sx={{ flex: '1 0 auto' }}>
               <Typography variant="h6">Store Owner</Typography>
          <Typography>Gizem Candemir</Typography>
             <Typography> Last login was Wednesday, February 23, 2022 11.38</Typography> 
             </CardContent>
-         
           </Box>
-
+    
          </Card>
 
-       
-       <img src="/sidebarIcon/account.png" width={24} />
       
 
        </Grid>
-       
+    
        <Grid item xs={12} md={12}>
-       <Typography>User (0,50</Typography>
+         <Card sx={{ display: 'flex' }}>
+        
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <CardContent sx={{ flex: '1 0 auto' }}>
+              <Typography variant="h6">User (0,50)</Typography>
+         <Typography>Customize what your staff members can edit and access</Typography>
+         <TableHeader data={data}  />
 
-     </Grid>
+            </CardContent>
+          </Box>
+    
+         </Card>
+
+      
+
+       </Grid>
 
     </Grid>
 
